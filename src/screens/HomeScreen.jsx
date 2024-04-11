c182953f4c09f542eec5de7650bee20cd109dafe
fardin abdul <119972067+fardin5@users.noreply.github.com>
2024-04-11 12:58:09 -0600

    changes

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
-    backgroundColor: '#1170FF',
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 30,
